191202206619a3773fcaae2b1f498d0c02bef962
rpanchal-depaul <rpancha2@depaul.edu>
2022-10-17 13:08:56 -0500

    Updated ruby version to 3.1.2.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 Rails.application.routes.draw do
-  resources :course_enrollments
-  resources :students
-  resources :courses
+
+  scope :admin do
+    resources :course_enrollments
+    resources :students
+    resources :courses
+  end
+
+  scope :api, constraints: lambda { |req| req.format == :json } do
+    resources :course_enrollments
+    resources :students
+    resources :courses
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
